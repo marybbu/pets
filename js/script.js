2065f3d0728725aa45794a2d8ccd2af60ceaeb96
MaryK <kuzaukovamaria@gmail.com>
2023-03-29 13:45:04 +0300

    mobilemenujs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("hii")
+const buttonsArray = document.querySelectorAll(".main_button")
+const menu = document.querySelector(".main_menu")
+
+function toggleMenu() {
+    menu.classList.toggle("open")
+}
+buttonsArray.forEach(button => {
+    button.addEventListener("click", toggleMenu)
+})
+
